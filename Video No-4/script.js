836412af5832454690554e032f346dd1787aead7
SAYAN-DAN <sayan94.arambagh@gmail.com>
2023-11-27 08:08:58 +0530

    Add Final Commit

--- Video No-4/script.js
+++ Video No-4/script.js
@@ -1,8 +1,39 @@
 var tl = gsap.timeline();
 
+tl.from("#nav", {
+  opacity: 0,
+  duration: 0.5,
+  delay: 0.3,
+});
 tl.from("#nav h1 ,#nav h4", {
   y: -100,
   stagger: 0.3,
   duration: 0.5,
   delay: 0.5,
+});
+
+tl.from("#left h1", {
+  opacity: 0,
+  x: -200,
+  duration: 0.5,
+  stagger: 0.5,
+});
+tl.from("#right img", {
+  scale: 2,
+  opacity: 0,
+  duration: 0.5,
+});
+
+gsap.from("#page2 .box", {
+  scale: 0,
+  opacity: 0,
+  duration: 0.5,
+  stagger: 0.5,
+  scrollTrigger: {
+    trigger: "#page2 .box",
+    scroller: "body",
+    // markers:true,
+    start:"top 70%"
+
+  },
 });
