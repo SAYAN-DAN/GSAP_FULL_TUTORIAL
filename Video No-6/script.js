5ec37cf937e9131fda3a245587bb287e4ab45edf
SAYAN-DAN <sayan94.arambagh@gmail.com>
2023-11-28 20:32:50 +0530

    Add Final Commit

--- Video No-6/script.js
+++ Video No-6/script.js
@@ -1,3 +1,32 @@
+var tl = gsap.timeline();
+
+let a = 0;
+function time() {
+  setInterval(() => {
+    a = a + Math.floor(Math.random() * 15);
+    if (a < 100) {
+      document.querySelector("#loader h1").innerHTML = a + "%";
+    } else {
+      a = 100;
+      document.querySelector("#loader h1").innerHTML = a + "%";
+    }
+  }, 100);
+}
+
+tl.to("#loader h1", {
+  scale: 1.5,
+  duration: 1,
+  delay: 0.3,
+  onStart: time(),
+});
+
+tl.to("#loader", {
+  // y:"-100vh",
+  top: "-100vh",
+  delay: 0.5,
+  duration: 1,
+});
+
 gsap.to("#page1 h1", {
   transform: "translateX(-100%)",
   fontWeight: "100",
